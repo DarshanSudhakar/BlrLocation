0f08f8f7c002d5140e156e8f5acf68aa9cb8216c
Darshan Sudhakar <darshanraj.shetty@gmail.com>
2021-01-10 23:46:14 +0530

    Changing the URL

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,8 @@
 exports.createPages = ({ actions }) => {
     const { createRedirect } = actions;
     createRedirect({
-      fromPath: "https://tvkds.netlify.app/*", 
-      toPath: "https://www.google.com/maps/search/?api=1&query=13.1693678,76.6674799&query_place_id=ChIJcX4LrJTirzsRRHmJXFl966Q", 
+      fromPath: "https://blrds.netlify.app/*", 
+      toPath: "https://www.google.com/maps/search/?api=1&query=13.1693678,76.6674799&query_place_id=ChIJMfDZz-8VrjsRvIgSiTv6RWc", 
       isPermanent: true, 
       force: true
     });
