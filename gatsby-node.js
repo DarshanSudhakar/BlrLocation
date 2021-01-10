04446f10a68639aa7c63db23149a429b65e08a46
DarshanSudhakar <$K3Ug1i&>
2021-01-10 23:01:49 +0530

    First commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createPages = ({ actions }) => {
+    const { createRedirect } = actions;
+    createRedirect({
+      fromPath: "https://tvkds.netlify.com/*", 
+      toPath: "https://www.google.com/maps/search/?api=1&query=13.1693678,76.6674799&query_place_id=ChIJcX4LrJTirzsRRHmJXFl966Q", 
+      isPermanent: true, 
+      force: true
+    });
+  };
